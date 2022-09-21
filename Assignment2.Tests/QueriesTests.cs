df285b49e072f9252a35d095121b22222706e714
memr <memr@itu.dk>
2022-09-21 16:15:28 +0200

    linqComplete

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,8 +3,71 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void GetJKRowlingWizard()
     {
+        // Arrange
+        Queries queries = new Queries();
 
+        // Act
+        var wizName = queries.getWizards("J.K Rowling");
+        //wizName.GetEnumerator().MoveNext();
+        var acWL = new List<string>
+        {
+            "Harry Potter",
+            "Ron Weasley"
+        };
+
+        // Assert
+        Assert.Equal(acWL, wizName);
     }
+
+    [Fact]
+    public void GetYearOfTheFirstSithLord()
+    {
+        // Arrange
+        Queries queries = new Queries();
+        // Act
+        int wizName = queries.getYearOFTheFirstSithLord();
+        // Assert
+        Assert.Equal(1977, wizName);
+    }
+    [Fact]
+    public void GetJKRowlingWizardsTest()
+    {
+        // Arrange
+        Queries queries = new Queries();
+        var tupleList = new List<Tuple<string, string>>
+        {
+            Tuple.Create("Harry Potter", "1997"),
+            Tuple.Create("Ron Weasley", "1997")
+        };
+        // Act
+        var wizList = queries.getListOfHPWizards().ToList();
+        // Assert
+        Assert.Equal(tupleList, wizList);
+    }
+
+    [Fact]
+    public void GetCreatorAndWizardNameTest()
+    {
+        // Arrange
+        Queries queries = new Queries();
+        var tupleList = new List<Tuple<string, string>>
+        {
+            Tuple.Create("J.R.R. Tolkien","Sauron"),
+            Tuple.Create("J.R.R. Tolkien","Gandalf"),
+            Tuple.Create("J.K Rowling","Harry Potter"),
+            Tuple.Create("J.K Rowling","Ron Weasley"),
+            Tuple.Create("George Lucas","Darth Vader"),
+            Tuple.Create("George Lucas","Darth Maul"),
+            Tuple.Create("George Lucas","Obi-Wan Kenobi"),
+            Tuple.Create("George Lucas","Qui-Gon Jinn"),
+            Tuple.Create("George Lucas","Luke Skywalker"),
+            Tuple.Create("George Lucas","Anakin Skywalker")
+        };
+        // Act
+        var wizList = queries.getCreatorNameAndWizard().ToList();
+        // Assert
+        Assert.Equal(tupleList, wizList);
+    }  
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,53 @@ namespace Assignment2;
 
 public class Queries
 {
-}
+    public IEnumerable<string> getWizards(string authorName)
+    {
+        var wizardList = WizardCollection.Create(); 
+        
+        var rowlingWizards = (
+            from wizard in wizardList
+            where wizard.Creator == authorName
+            select wizard.Name
+        ).ToList();
+        return rowlingWizards;
+    }
+
+    public int getYearOFTheFirstSithLord()
+    {
+        var wizardList = WizardCollection.Create(); 
+
+        var sithWizards = (
+            (from wizard in wizardList
+            where wizard.Creator == "George Lucas" && wizard.Name.Contains("Darth")
+            select wizard.Year)
+            .Min()
+        );
+        return (int)sithWizards!;
+    }
+
+    public IEnumerable<Tuple<string, string>> getListOfHPWizards()
+    {
+        var wizardList = WizardCollection.Create(); 
+
+        var rowlingWizards = (
+            from wizard in wizardList
+            where wizard.Creator == "J.K Rowling"
+            select new Tuple<string, string>(wizard.Name, wizard.Year.ToString()!)
+        ).ToList();
+        return rowlingWizards;
+    }
+
+    //List of wizard names grouped by creator in reverse order by creator name and then wizard name.
+    public IEnumerable<Tuple<string, string>> getCreatorNameAndWizard()
+    {
+        var wizardList = WizardCollection.Create(); 
+
+        var rowlingWizards = (
+            from wizard in wizardList
+            orderby wizard.Creator descending
+            select new Tuple<string, string>(wizard.Creator, wizard.Name)
+        ).ToList();
+        return rowlingWizards;
+    }
+}    
